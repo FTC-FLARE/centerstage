42ebafe401014aecda376ac133931d691ffdd131
ftc12833-p2 <ftc12833p2@gmail.com>
2024-01-18 18:24:08 -0500

    add launcher

--- src/main/java/org/firstinspires/ftc/mmcenterstage/MM_Launcher.java
+++ src/main/java/org/firstinspires/ftc/mmcenterstage/MM_Launcher.java
@@ -1,29 +1,46 @@
 package org.firstinspires.ftc.mmcenterstage;
 
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.ServoControllerEx;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 public class MM_Launcher {
     MM_OpMode opMode;
 
     private Servo droneRelease = null;
     private Servo lift = null;
+    ElapsedTime liftTime = new ElapsedTime();
+
+    private boolean inLaunchPosition = false;
+    private boolean liftRequested = false;
 
     public MM_Launcher(MM_OpMode opMode) {
         this.opMode = opMode;
+        init();
     }
-//
-//    public void launch() {
-//        if (MM_TeleOp.matchTimer.time() > 90 && MM_LiftLift.isInLaunchPos) {
-//            if (opMode.gamepad2.right_trigger > 0.1) {
-//                droneReleaser.setPosition(1);
-//            }
-//        }
-//    }
-//
+
+    public void launchControl(){
+        if (MM_TeleOp.currentGamepad1.dpad_up && !MM_TeleOp.previousGamepad1.dpad_up){
+            lift.setPosition(.55);
+            liftTime.reset();
+            liftRequested = true;
+        }
+        if (liftRequested && !inLaunchPosition && liftTime.time() >= .75){
+            ServoControllerEx controller = (ServoControllerEx) lift.getController();
+            controller.setServoPwmDisable(lift.getPortNumber());
+
+            inLaunchPosition = true;
+        }
+        if (inLaunchPosition && (MM_TeleOp.currentGamepad1.dpad_down && !MM_TeleOp.previousGamepad1.dpad_down)){
+            droneRelease.setPosition(1);
+        }
+    }
+
     public void init() {
         droneRelease = opMode.hardwareMap.get(Servo.class, "droneRelease");
         droneRelease.setPosition(0);
 
         lift = opMode.hardwareMap.get(Servo.class, "droneLift");
+        lift.setPosition(0);
     }
 }
